28190c9d53eb48efeb4754aaad204fc6784ff7b8
Henry Garner <henry@henrygarner.com>
2024-11-19 20:21:48 +0000

    Obviate need to download sample data

--- script/download-data.sh
+++ script/download-data.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 
-script_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-data_dir="${script_dir}/../data"
-uk_url="http://www.complex-systems.meduniwien.ac.at/elections/ElectionData/UK2010.xls"
-ru_url="http://www.complex-systems.meduniwien.ac.at/elections/ElectionData/Russia2011.zip"
-
-mkdir -p "${data_dir}"
-
-echo "Downloading ${uk_url}..."
-if [ $(curl -s --head -w %{http_code} $uk_url -o /dev/null) -eq 200 ]; then
-    curl -o "${data_dir}/UK2010.xls" $uk_url
-else
-    echo "Couldn't download UK data. Perhaps it has moved? Consult http://wiki.clojuredatascience.com"
-fi
-
-echo "Downloading ${ru_url}..."
-if [ $(curl -s --head -w %{http_code} $ru_url -o /dev/null) -eq 200 ]; then
-    curl -o "${data_dir}/Russia2011.zip" $ru_url
-    unzip "${data_dir}/Russia2011.zip" -d "${data_dir}"
-else
-    echo "Couldn't download Russia data. Perhaps it has moved? Consult http://wiki.clojuredatascience.com"
-fi
+echo "It is no longer necessary to download the sample data, it has been included in this project's data directory."
+echo "Skip ahead to the next section to begin analysing it!"
